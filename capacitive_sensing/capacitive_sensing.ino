846afaa5d6be380dcdff169dcc6f568363d0472d
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2015-08-02 08:59:26 +0300

    Fix comments, remove USE_SERIAL

--- capacitive_sensing/capacitive_sensing.ino
+++ capacitive_sensing/capacitive_sensing.ino
@@ -4,42 +4,43 @@
 #include <JeeLib.h>
 
 /*
- * Connections for radio transmitter:
+ * Connections for 433MHz radio transmitter:
  *  - Arduino 5V -> Tx VCC
  *  - Arduino GND -> Tx GND
  *  - Arduino D7 -> Tx DATA
+ *  
+ * Connections for capacitive sensing:
+ *  - Big resistor between D2 & D3 (> 1 MOhm seems to work)
+ *  - Sensor's first wire (e.g. from aluminium foil) to D3
+ *  - Sensor's other wire to GND
  */
 
 
-CapacitiveSensor cs = CapacitiveSensor(2,3);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
+CapacitiveSensor cs = CapacitiveSensor(2,3);        // 10 megohm resistor between pins D2 & D3. D3 is the sensor pin.
 RH_ASK driver(2000, 8, 7, 6, false);
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
-bool USE_SERIAL = false;
 
 void setup()                    
 {
-   cs.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   cs.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate
    Serial.begin(9600);
    if (!driver.init())
      Serial.println("init failed");
-   // initialize digital pin 13 as an output.
    pinMode(13, OUTPUT);
 }
 
 void loop()                    
 {
-    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(13, HIGH);   // turn the built-in LED on
     long start = millis();
     long total1 =  cs.capacitiveSensor(100);
     long total2 =  cs.capacitiveSensorRaw(100);
 
-    if(USE_SERIAL) {
-      Serial.print(millis() - start);        // check on performance in milliseconds
-      Serial.print(": ");
-      Serial.print(total1);                  // print sensor output 1
-      Serial.print("\t\t");
-      Serial.println(total2);
-    }
+    Serial.print(millis() - start);        // check on performance in milliseconds
+    Serial.print(": ");
+    Serial.print(total1);                  // print sensor output 1
+    Serial.print("\t\t");
+    Serial.println(total2);
 
     char buffer[30];
     sprintf(buffer, "val: %ld raw: %ld", total1, total2);
