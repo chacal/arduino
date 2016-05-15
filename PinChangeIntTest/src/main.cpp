bfc9f9a1e54ade0ff98ead32bebacd83ed224a66
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-05-15 08:55:41 +0300

    Use powerDown() from own Utils library

--- PinChangeIntTest/src/main.cpp
+++ PinChangeIntTest/src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
-#include <avr/sleep.h>
-#include <util/atomic.h>
+#include <power.h>
 
 #include "main.h"
 #include "buttons.h"
@@ -35,20 +34,4 @@ void goToSleep() {
   Serial.flush();
   powerDown();
   wakeUpTime = millis();
-}
-
-void powerDown() {
-  ADCSRA &= ~ bit(ADEN); // disable the ADC
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  ATOMIC_BLOCK(ATOMIC_FORCEON) {
-    sleep_enable();
-#ifdef BODSE
-    MCUCR = MCUCR | bit(BODSE) | bit(BODS); // timed sequence
-    MCUCR = (MCUCR & ~ bit(BODSE)) | bit(BODS);
-#endif
-  }
-
-  sleep_cpu();
-
-  sleep_disable();
 }
