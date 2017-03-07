ff3d3c59bbeb06078be9eeb3437f00f2320dc5b6
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-03-07 13:04:49 +0200

    Moved to a separate repository

--- pio-libs/ESP-MQTT-utils/src/ESP-MQTT-utils.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "MQTTConfigSaver.h"
-#include "NetworkConnectionManager.h"
--- pio-libs/ESP-MQTT-utils/src/NetworkConnectionManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <WiFiManager.h>
-#include "MQTTConfigSaver.h"
-#include <PubSubClient.h>
-#include "StreamPrint.h"
-
-void connectWiFi(WiFiManager &wifiManager, MqttConfiguration &mqttConfig, void (*saveConfigCallback)());
-void connectMQTT(PubSubClient &mqttClient, MqttConfiguration &mqttConfig, Client &client, Stream &stream, MQTT_CALLBACK_SIGNATURE);
