0d4f1eca00d121095a0f41c31ea64cc6c78b2f34
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-08-13 21:22:12 +0300

    Enable UART logging
    Use pin 5 for TX, 6 for RX. 115200 baud.

--- experiments/nrf51-logging-test/main.c
+++ experiments/nrf51-logging-test/main.c
@@ -2,18 +2,23 @@
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
 
 #define LED_PIN 3
 
 
 int main(void) {
+  (void) NRF_LOG_INIT(NULL);
+
   nrf_gpio_cfg_output(LED_PIN);
   nrf_gpio_pin_write(LED_PIN, 0);
 
   /* Toggle LEDs. */
   while(true) {
+    NRF_LOG_INFO("Blink!\n");
     nrf_gpio_pin_toggle(LED_PIN);
-    nrf_delay_ms(1000);
+    nrf_delay_ms(2000);
   }
 }
 
--- experiments/nrf51-logging-test/sdk_config.h
+++ experiments/nrf51-logging-test/sdk_config.h
@@ -3475,7 +3475,7 @@
 // <e> NRF_LOG_ENABLED - nrf_log - Logging
 //==========================================================
 #ifndef NRF_LOG_ENABLED
-#define NRF_LOG_ENABLED 0
+#define NRF_LOG_ENABLED 1
 #endif
 #if  NRF_LOG_ENABLED
 // <e> NRF_LOG_USES_COLORS - If enabled then ANSI escape code for colors is prefixed to every string
@@ -3552,7 +3552,7 @@
 // <i> Log data is buffered and can be processed in idle.
 //==========================================================
 #ifndef NRF_LOG_DEFERRED
-#define NRF_LOG_DEFERRED 1
+#define NRF_LOG_DEFERRED 0
 #endif
 #if  NRF_LOG_DEFERRED
 // <o> NRF_LOG_DEFERRED_BUFSIZE - Size of the buffer for logs in words. 
@@ -3625,12 +3625,12 @@
 
 // <o> NRF_LOG_BACKEND_SERIAL_UART_TX_PIN - UART TX pin 
 #ifndef NRF_LOG_BACKEND_SERIAL_UART_TX_PIN
-#define NRF_LOG_BACKEND_SERIAL_UART_TX_PIN 9
+#define NRF_LOG_BACKEND_SERIAL_UART_TX_PIN 5
 #endif
 
 // <o> NRF_LOG_BACKEND_SERIAL_UART_RX_PIN - UART RX pin 
 #ifndef NRF_LOG_BACKEND_SERIAL_UART_RX_PIN
-#define NRF_LOG_BACKEND_SERIAL_UART_RX_PIN 11
+#define NRF_LOG_BACKEND_SERIAL_UART_RX_PIN 6
 #endif
 
 // <o> NRF_LOG_BACKEND_SERIAL_UART_RTS_PIN - UART RTS pin 
