b5930e277fe9711e2adf4ba134f49f956a0e437a
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-08-18 00:04:57 +0300

    Initialize GAP parameters

--- experiments/nrf51-logging-test/src/main.c
+++ experiments/nrf51-logging-test/src/main.c
@@ -9,6 +9,12 @@
 #define CENTRAL_LINK_COUNT              0
 #define PERIPHERAL_LINK_COUNT           1
 
+#define DEVICE_NAME                     "Nordic_UART"                               /**< Name of device. Will be included in the advertising data. */
+#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(20, UNIT_1_25_MS)             /**< Minimum acceptable connection interval (20 ms), Connection interval uses 1.25 ms units. */
+#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(75, UNIT_1_25_MS)             /**< Maximum acceptable connection interval (75 ms), Connection interval uses 1.25 ms units. */
+#define SLAVE_LATENCY                   0                                           /**< Slave latency. */
+#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)             /**< Connection supervisory timeout (4 seconds), Supervision Timeout uses 10 ms units. */
+
 
 static void ble_evt_dispatch(ble_evt_t * p_ble_evt) {
 }
@@ -41,16 +47,45 @@ static void ble_stack_init(void) {
 }
 
 
+/**@brief Function for the GAP initialization.
+ *
+ * @details This function will set up all the necessary GAP (Generic Access Profile) parameters of
+ *          the device. It also sets the permissions and appearance.
+ */
+static void gap_params_init(void)
+{
+  uint32_t                err_code;
+  ble_gap_conn_params_t   gap_conn_params;
+  ble_gap_conn_sec_mode_t sec_mode;
+
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+
+  err_code = sd_ble_gap_device_name_set(&sec_mode, (const uint8_t *) DEVICE_NAME, strlen(DEVICE_NAME));
+  APP_ERROR_CHECK(err_code);
+
+  memset(&gap_conn_params, 0, sizeof(gap_conn_params));
+
+  gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
+  gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
+  gap_conn_params.slave_latency     = SLAVE_LATENCY;
+  gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
+
+  err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
+  APP_ERROR_CHECK(err_code);
+}
+
+
 int main(void) {
   (void) NRF_LOG_INIT(NULL);
   bsp_board_leds_init();
 
   ble_stack_init();
+  gap_params_init();
 
   /* Toggle LEDs. */
   while(true) {
     NRF_LOG_INFO("Blink!\n");
     bsp_board_led_invert(BSP_LED_0);
-    nrf_delay_ms(500);
+    nrf_delay_ms(1000);
   }
 }
