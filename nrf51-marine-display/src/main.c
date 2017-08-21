ad49e36058b1c7ac90f5496eab0aa06480abf4b9
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-08-21 08:31:35 +0300

    Add first version of nrf51-marine-display

---
+++ nrf51-marine-display/src/main.c
@@ -0,0 +1,288 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "softdevice_handler.h"
+#include "nrf_delay.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "bsp.h"
+#include "ble_nus.h"
+#include "ble_advertising.h"
+#include "ble_advdata.h"
+#include "ble_hci.h"
+#include "app_timer.h"
+
+
+#define CENTRAL_LINK_COUNT              0
+#define PERIPHERAL_LINK_COUNT           1
+#define APP_FEATURE_NOT_SUPPORTED       BLE_GATT_STATUS_ATTERR_APP_BEGIN + 2        /**< Reply when unsupported features are requested. */
+
+#define DEVICE_NAME                     "Nordic_UART"                               /**< Name of device. Will be included in the advertising data. */
+#define NUS_SERVICE_UUID_TYPE           BLE_UUID_TYPE_VENDOR_BEGIN                  /**< UUID type for the Nordic UART Service (vendor specific). */
+
+#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(50, UNIT_1_25_MS)             /**< Minimum acceptable connection interval (20 ms), Connection interval uses 1.25 ms units. */
+#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS)            /**< Maximum acceptable connection interval (75 ms), Connection interval uses 1.25 ms units. */
+#define SLAVE_LATENCY                   10                                          /**< Slave latency. */
+#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)             /**< Connection supervisory timeout (4 seconds), Supervision Timeout uses 10 ms units. */
+
+#define APP_ADV_INTERVAL                MSEC_TO_UNITS(50, UNIT_0_625_MS)            /**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
+#define APP_ADV_TIMEOUT_IN_SECONDS      60                                          /**< The advertising timeout (in units of seconds). */
+#define APP_ADV_SLOW_INTERVAL           MSEC_TO_UNITS(100, UNIT_0_625_MS)           /**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
+#define APP_ADV_SLOW_TIMEOUT_IN_SECONDS 0                                           /**< The advertising timeout (in units of seconds). */
+
+static ble_nus_t                        m_nus;                                      /**< Structure to identify the Nordic UART Service. */
+static uint16_t                         m_conn_handle = BLE_CONN_HANDLE_INVALID;    /**< Handle of the current connection. */
+static ble_uuid_t                       m_adv_uuids[] = {{BLE_UUID_NUS_SERVICE, NUS_SERVICE_UUID_TYPE}};  /**< Universally unique service identifier. */
+
+/**@brief Function for the application's SoftDevice event handler.
+ *
+ * @param[in] p_ble_evt SoftDevice event.
+ */
+static void on_ble_evt(ble_evt_t * p_ble_evt) {
+  uint32_t err_code;
+
+  switch (p_ble_evt->header.evt_id) {
+    case BLE_GAP_EVT_CONNECTED:
+      m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+      break; // BLE_GAP_EVT_CONNECTED
+
+    case BLE_GAP_EVT_DISCONNECTED:
+      m_conn_handle = BLE_CONN_HANDLE_INVALID;
+      break; // BLE_GAP_EVT_DISCONNECTED
+
+    case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
+      // Pairing not supported
+      err_code = sd_ble_gap_sec_params_reply(m_conn_handle, BLE_GAP_SEC_STATUS_PAIRING_NOT_SUPP, NULL, NULL);
+      APP_ERROR_CHECK(err_code);
+      break; // BLE_GAP_EVT_SEC_PARAMS_REQUEST
+
+    case BLE_GATTS_EVT_SYS_ATTR_MISSING:
+      // No system attributes have been stored.
+      err_code = sd_ble_gatts_sys_attr_set(m_conn_handle, NULL, 0, 0);
+      APP_ERROR_CHECK(err_code);
+      break; // BLE_GATTS_EVT_SYS_ATTR_MISSING
+
+    case BLE_GATTC_EVT_TIMEOUT:
+      // Disconnect on GATT Client timeout event.
+      err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gattc_evt.conn_handle, BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+      APP_ERROR_CHECK(err_code);
+      break; // BLE_GATTC_EVT_TIMEOUT
+
+    case BLE_GATTS_EVT_TIMEOUT:
+      // Disconnect on GATT Server timeout event.
+      err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gatts_evt.conn_handle, BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+      APP_ERROR_CHECK(err_code);
+      break; // BLE_GATTS_EVT_TIMEOUT
+
+    case BLE_EVT_USER_MEM_REQUEST:
+      err_code = sd_ble_user_mem_reply(p_ble_evt->evt.gattc_evt.conn_handle, NULL);
+      APP_ERROR_CHECK(err_code);
+      break; // BLE_EVT_USER_MEM_REQUEST
+
+    case BLE_GATTS_EVT_RW_AUTHORIZE_REQUEST: {
+      ble_gatts_evt_rw_authorize_request_t  req;
+      ble_gatts_rw_authorize_reply_params_t auth_reply;
+
+      req = p_ble_evt->evt.gatts_evt.params.authorize_request;
+
+      if (req.type != BLE_GATTS_AUTHORIZE_TYPE_INVALID) {
+        if ((req.request.write.op == BLE_GATTS_OP_PREP_WRITE_REQ)     ||
+            (req.request.write.op == BLE_GATTS_OP_EXEC_WRITE_REQ_NOW) ||
+            (req.request.write.op == BLE_GATTS_OP_EXEC_WRITE_REQ_CANCEL)) {
+          if (req.type == BLE_GATTS_AUTHORIZE_TYPE_WRITE) {
+            auth_reply.type = BLE_GATTS_AUTHORIZE_TYPE_WRITE;
+          } else {
+            auth_reply.type = BLE_GATTS_AUTHORIZE_TYPE_READ;
+          }
+          auth_reply.params.write.gatt_status = APP_FEATURE_NOT_SUPPORTED;
+          err_code = sd_ble_gatts_rw_authorize_reply(p_ble_evt->evt.gatts_evt.conn_handle,
+                                                     &auth_reply);
+          APP_ERROR_CHECK(err_code);
+        }
+      }
+    } break; // BLE_GATTS_EVT_RW_AUTHORIZE_REQUEST
+
+    default:
+      break;
+  }
+}
+
+
+static void ble_evt_dispatch(ble_evt_t * p_ble_evt) {
+  ble_nus_on_ble_evt(&m_nus, p_ble_evt);
+  on_ble_evt(p_ble_evt);
+  ble_advertising_on_ble_evt(p_ble_evt);
+}
+
+
+/**@brief Function for the SoftDevice initialization.
+ *
+ * @details This function initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void) {
+  uint32_t err_code;
+  nrf_clock_lf_cfg_t clock_lf_cfg = NRF_CLOCK_LFCLKSRC;
+
+  // Initialize SoftDevice.
+  SOFTDEVICE_HANDLER_INIT(&clock_lf_cfg, NULL);
+
+  ble_enable_params_t ble_enable_params;
+  err_code = softdevice_enable_get_default_config(CENTRAL_LINK_COUNT, PERIPHERAL_LINK_COUNT, &ble_enable_params);
+  APP_ERROR_CHECK(err_code);
+
+  //Check the ram settings against the used number of links
+  CHECK_RAM_START_ADDR(CENTRAL_LINK_COUNT,PERIPHERAL_LINK_COUNT);
+
+  err_code = softdevice_enable(&ble_enable_params);
+  APP_ERROR_CHECK(err_code);
+
+  // Subscribe for BLE events.
+  err_code = softdevice_ble_evt_handler_set(ble_evt_dispatch);
+  APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for the GAP initialization.
+ *
+ * @details This function will set up all the necessary GAP (Generic Access Profile) parameters of
+ *          the device. It also sets the permissions and appearance.
+ */
+static void gap_params_init(void) {
+  uint32_t                err_code;
+  ble_gap_conn_params_t   gap_conn_params;
+  ble_gap_conn_sec_mode_t sec_mode;
+
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+
+  err_code = sd_ble_gap_device_name_set(&sec_mode, (const uint8_t *) DEVICE_NAME, strlen(DEVICE_NAME));
+  APP_ERROR_CHECK(err_code);
+
+  memset(&gap_conn_params, 0, sizeof(gap_conn_params));
+
+  gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
+  gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
+  gap_conn_params.slave_latency     = SLAVE_LATENCY;
+  gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
+
+  err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
+  APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for handling the data from the Nordic UART Service.
+ *
+ * @details This function will process the data received from the Nordic UART BLE Service and send
+ *          it to the UART module.
+ *
+ * @param[in] p_nus    Nordic UART Service structure.
+ * @param[in] p_data   Data to be send to UART module.
+ * @param[in] length   Length of the data.
+ */
+static void nus_data_handler(ble_nus_t * p_nus, uint8_t * p_data, uint16_t length) {
+  NRF_LOG_INFO("Got %d bytes of data\n", length);
+}
+
+
+/**@brief Function for initializing services that will be used by the application.
+ */
+static void services_init(void) {
+  uint32_t       err_code;
+  ble_nus_init_t nus_init;
+
+  memset(&nus_init, 0, sizeof(nus_init));
+  nus_init.data_handler = nus_data_handler;
+
+  err_code = ble_nus_init(&m_nus, &nus_init);
+  APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for handling advertising events.
+ *
+ * @details This function will be called for advertising events which are passed to the application.
+ *
+ * @param[in] ble_adv_evt  Advertising event.
+ */
+static void on_adv_evt(const ble_adv_evt_t ble_adv_evt) {
+}
+
+
+/**@brief Function for initializing the Advertising functionality.
+ */
+static void advertising_init(void) {
+  uint32_t               err_code;
+  ble_advdata_t          advdata;
+  ble_advdata_t          scanrsp;
+  ble_adv_modes_config_t options;
+
+  // Build advertising data struct to pass into @ref ble_advertising_init.
+  memset(&advdata, 0, sizeof(advdata));
+  advdata.name_type          = BLE_ADVDATA_FULL_NAME;
+  advdata.include_appearance = false;
+  advdata.flags              = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
+
+  memset(&scanrsp, 0, sizeof(scanrsp));
+  scanrsp.uuids_complete.uuid_cnt = sizeof(m_adv_uuids) / sizeof(m_adv_uuids[0]);
+  scanrsp.uuids_complete.p_uuids  = m_adv_uuids;
+
+  memset(&options, 0, sizeof(options));
+  options.ble_adv_fast_enabled  = true;
+  options.ble_adv_fast_interval = APP_ADV_INTERVAL;
+  options.ble_adv_fast_timeout  = APP_ADV_TIMEOUT_IN_SECONDS;
+  options.ble_adv_slow_enabled  = true;
+  options.ble_adv_slow_interval = APP_ADV_SLOW_INTERVAL;
+  options.ble_adv_slow_timeout  = APP_ADV_SLOW_TIMEOUT_IN_SECONDS;
+
+  err_code = ble_advertising_init(&advdata, &scanrsp, &options, on_adv_evt, NULL);
+  APP_ERROR_CHECK(err_code);
+}
+
+
+static void power_manage(void) {
+  uint32_t err_code = sd_app_evt_wait();
+  APP_ERROR_CHECK(err_code);
+}
+
+
+static void on_app_timer(void* pContext) {
+  if(m_nus.conn_handle != BLE_CONN_HANDLE_INVALID && m_nus.is_notification_enabled) {
+    char test[] = "Hello world!";
+
+    uint32_t err_code = ble_nus_string_send(&m_nus, (uint8_t *)test, sizeof(test));
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_INFO("Sent data!\n");
+  }
+}
+
+static void nus_transfer_start() {
+  APP_TIMER_INIT(0, 4, NULL);
+
+  APP_TIMER_DEF(timer);
+  uint32_t err_code = app_timer_create(&timer, APP_TIMER_MODE_REPEATED, on_app_timer);
+  APP_ERROR_CHECK(err_code);
+
+  err_code = app_timer_start(timer, APP_TIMER_TICKS(500, 0), NULL);
+  APP_ERROR_CHECK(err_code);
+
+  NRF_LOG_INFO("App timer started!\n");
+}
+
+
+int main(void) {
+  (void) NRF_LOG_INIT(NULL);
+  bsp_board_leds_init();
+
+  ble_stack_init();
+  gap_params_init();
+  services_init();
+  advertising_init();
+  uint32_t err_code = ble_advertising_start(BLE_ADV_MODE_FAST);
+  APP_ERROR_CHECK(err_code);
+  NRF_LOG_INFO("Advertising started!\n");
+
+  nus_transfer_start();
+
+  while(true) {
+    power_manage();
+  }
+}
