7627d423efd213f5eb0b5f839e44b8ebf0a8c7cc
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-10-29 21:21:46 +0200

    Clean up test sensor

--- experiments/nrf5-sensor-test/src/main.c
+++ experiments/nrf5-sensor-test/src/main.c
@@ -1,7 +1,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <ble/common/ble_advdata.h>
-#include "ble_advdata.h"
 #include "nordic_common.h"
 #include "softdevice_handler.h"
 #include "bsp.h"
@@ -13,12 +12,14 @@
 #define PERIPHERAL_LINK_COUNT           0                                 /**< Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
 
 #define APP_CFG_NON_CONN_ADV_TIMEOUT    0                                 /**< Time for which the device must be advertising in non-connectable mode (in seconds). 0 disables timeout. */
-#define NON_CONNECTABLE_ADV_INTERVAL    MSEC_TO_UNITS(1000, UNIT_0_625_MS) /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100ms to 10.24s). */
+#define NON_CONNECTABLE_ADV_INTERVAL    MSEC_TO_UNITS(3000, UNIT_0_625_MS) /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100ms to 10.24s). */
 
 #define DEVICE_NAME                     "S035"
 
 #define APP_TIMER_PRESCALER             0                                 /**< Value of the RTC1 PRESCALER register. */
 #define APP_TIMER_OP_QUEUE_SIZE         4                                 /**< Size of timer operation queues. */
+
+#define TX_POWER_LEVEL                  4                                 /** Max power, +4dBm */
 
 static ble_gap_adv_params_t m_adv_params;                                 /**< Parameters to be passed to the stack when starting advertising. */
 
@@ -52,7 +53,7 @@ static void advertising_init(void) {
   sensor_data.ttl         = 2;
   sensor_data.tag         = 'm';
   sensor_data.temperature = 2150;
-  sensor_data.humidity    = 4015;
+  sensor_data.humidity    = 5505;
   sensor_data.pressure    = 10153;
   sensor_data.vcc         = 4015;
   manuf_data.data.p_data  = (uint8_t *) &sensor_data;
@@ -79,60 +80,40 @@ static void advertising_init(void) {
 
 
 static void advertising_start(void) {
-  uint32_t err_code;
-  err_code = sd_ble_gap_adv_start(&m_adv_params);
-  APP_ERROR_CHECK(err_code);
+  APP_ERROR_CHECK(sd_ble_gap_adv_start(&m_adv_params));
 }
 
-
 static void ble_stack_init(void) {
-  uint32_t err_code;
-
   nrf_clock_lf_cfg_t clock_lf_cfg = NRF_CLOCK_LFCLKSRC;
-
   SOFTDEVICE_HANDLER_INIT(&clock_lf_cfg, NULL);
 
   ble_enable_params_t ble_enable_params;
-  err_code = softdevice_enable_get_default_config(CENTRAL_LINK_COUNT,
-                                                  PERIPHERAL_LINK_COUNT,
-                                                  &ble_enable_params);
-  APP_ERROR_CHECK(err_code);
+  APP_ERROR_CHECK(softdevice_enable_get_default_config(CENTRAL_LINK_COUNT, PERIPHERAL_LINK_COUNT, &ble_enable_params));
 
   CHECK_RAM_START_ADDR(CENTRAL_LINK_COUNT, PERIPHERAL_LINK_COUNT);
 
-  err_code = softdevice_enable(&ble_enable_params);
-  APP_ERROR_CHECK(err_code);
+  APP_ERROR_CHECK(softdevice_enable(&ble_enable_params));
+  APP_ERROR_CHECK(sd_ble_gap_tx_power_set(TX_POWER_LEVEL));
 }
 
 
-/**@brief Function for doing power management.
- */
 static void power_manage(void) {
   uint32_t err_code = sd_app_evt_wait();
   APP_ERROR_CHECK(err_code);
 }
 
 
-/**
- * @brief Function for application main entry.
- */
 int main(void) {
-  uint32_t err_code;
-  // Initialize.
-  err_code = NRF_LOG_INIT(NULL);
-  APP_ERROR_CHECK(err_code);
+  APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
 
   APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
-  APP_ERROR_CHECK(err_code);
   ble_stack_init();
   advertising_init();
 
-  NRF_LOG_INFO("BLE Beacon started\r\n");
+  NRF_LOG_INFO("BLE Beacon started\n");
   advertising_start();
 
   for(;;) {
-    if(NRF_LOG_PROCESS() == false) {
-      power_manage();
-    }
+    power_manage();
   }
 }
