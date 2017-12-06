9f4f3091ef4aca2b76db6eb8fc3ac3b378a90202
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-12-06 11:08:48 +0200

    Send received packets to UART

--- nrf5-bt-sensor-receiver/src/main.c
+++ nrf5-bt-sensor-receiver/src/main.c
@@ -1,13 +1,46 @@
 #include <nrf.h>
-#include <nrf_log.h>
-#include <nrf_log_ctrl.h>
 #include <app_error.h>
 #include <ble_gap.h>
-#include <stdlib.h>
+#include <string.h>
+#include <nrf_drv_uart.h>
 #include "util.h"
+#include "radio.h"
+
+#ifdef NRF51
+
+#include <sdk_config_nrf51/sdk_config.h>
+
+#else
+#include <sdk_config_nrf52/sdk_config.h>
+#endif
+
 
 #define FILTERED_MANUFACTURER_ID   0xDADA
+#define UART_TX_PIN                13
 
+nrf_drv_uart_t m_uart = NRF_DRV_UART_INSTANCE(UART0_INSTANCE_INDEX);
+
+
+void tohex(char *in, size_t insz, char *out, size_t outsz) {
+  char       *pin  = in;
+  const char *hex  = "0123456789ABCDEF";
+  char       *pout = out;
+
+  for(; pin < in + insz; pout += 2, pin++) {
+    pout[0] = hex[(*pin >> 4) & 0xF];
+    pout[1] = hex[*pin & 0xF];
+    if(pout + 2 - out > outsz) {
+      break;
+    }
+  }
+
+  pout[0] = 0;
+}
+
+void uart_send_str(char *str) {
+  uint8_t len = (uint8_t) strlen(str);
+  nrf_drv_uart_tx(&m_uart, (uint8_t *) str, len);
+}
 
 static void on_rx_adv_packet(nrf_radio_packet_t adv_packet) {
   adv_data_t manuf_data;
@@ -17,23 +50,33 @@ static void on_rx_adv_packet(nrf_radio_packet_t adv_packet) {
   if(res == NRF_SUCCESS) {
     uint16_t *manufacturer_id = (uint16_t *) &manuf_data.data[0];  // First two bytes are the manufacturer ID
     if(*manufacturer_id == FILTERED_MANUFACTURER_ID) {
-      NRF_LOG_INFO("RX\n");
-      // TODO: Write packet to UART here
+      char tx_buf[250];
+      char hex_buf[80];
+
+      tohex((char *) adv_packet.payload, adv_packet.payload_length, hex_buf, sizeof(hex_buf));
+      sprintf(tx_buf, "{\"data\": \"%s\", \"rssi\": %d}\n", hex_buf, 0);  // TODO: Get proper RSSI value
+
+      uart_send_str(tx_buf);
     }
   }
 }
 
+void uart_init() {
+  nrf_drv_uart_config_t config = NRF_DRV_UART_DEFAULT_CONFIG;
+  config.pseltxd = UART_TX_PIN;
+  APP_ERROR_CHECK(nrf_drv_uart_init(&m_uart, &config, NULL));
+}
 
 int main(void) {
-  APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
-  NRF_LOG_INFO("Starting..\n");
-
   util_start_clocks();
+  uart_init();
   radio_init(on_rx_adv_packet);
   radio_rx_start();
 
-  NRF_LOG_INFO("Started\n");
+  uart_send_str("BT sensor receiver started\n");
 
   for(;;) {
+    __WFE();
+    __WFI();
   }
 }
