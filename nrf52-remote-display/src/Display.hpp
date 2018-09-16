ca8242d50c4da7d46f744c6bfd86387662dfb616
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-09-16 17:09:48 +0300

    Reformat

--- nrf52-remote-display/src/Display.hpp
+++ nrf52-remote-display/src/Display.hpp
@@ -8,18 +8,24 @@ extern "C" {
 #include <u8g2.h>
 }
 
-using font_size = named_type<uint8_t , struct font_size_param>;
-
+using font_size = named_type<uint8_t, struct font_size_param>;
 
 class display {
 public:
   display();
+
   void on();
+
   void off();
+
   void draw_str(uint8_t x, uint8_t y, font_size, char *str);
+
   void draw_line(uint8_t start_x, uint8_t start_y, uint8_t end_x, uint8_t end_y);
+
   void render();
+
   void clear();
+
   uint8_t centered_x(const char *str, font_size);
 
 private:
