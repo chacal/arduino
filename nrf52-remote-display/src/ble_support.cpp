a802cbb8b9041ce135d1e4d20d0fe59bb576981a
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-09-22 23:30:58 +0300

    Don’t require connection params update, log update’s status

--- nrf52-remote-display/src/ble_support.cpp
+++ nrf52-remote-display/src/ble_support.cpp
@@ -82,8 +82,9 @@ namespace ble_support {
     cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
     cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
     cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
-    cp_init.disconnect_on_fail             = true;
+    cp_init.disconnect_on_fail             = false;
     cp_init.error_handler                  = [](uint32_t nrf_error) { APP_ERROR_HANDLER(nrf_error); };
+    cp_init.evt_handler                    = [](ble_conn_params_evt_t * p_evt) { NRF_LOG_INFO("Conn params evt: %d", p_evt->evt_type); };
 
     APP_ERROR_CHECK(ble_conn_params_init(&cp_init));
   }
