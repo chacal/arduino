6e669ddcbe41bde9e2cb5f601df485bd3b067eea
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-09-23 15:01:21 +0300

    Start advertising with whitelist in adv_with_whitelist state

--- nrf52-remote-display/src/states/adv_with_whitelist.hpp
+++ nrf52-remote-display/src/states/adv_with_whitelist.hpp
@@ -1,14 +1,30 @@
 #pragma once
 
 #include <nrf_log.h>
+#include <ble.h>
+#include <ble_support_adv.hpp>
 #include "common.hpp"
 
 using namespace fsm;
 
 namespace states {
+  struct connected;
+
   struct adv_with_whitelist : Base {
     virtual void enter(Context &context) {
-      NRF_LOG_INFO("Advertising with whitelist");
+      NRF_LOG_INFO("Connectable");
+      ble_support::adv::connectable_start();
     }
+
+    virtual void react(const ble_evt_t *p_ble_evt, Control &control, Context &context) {
+      switch (p_ble_evt->header.evt_id) {
+        case BLE_GAP_EVT_CONNECTED:
+          context.conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+          control.changeTo<connected>();
+          break;
+      }
+    }
+
+    using Base::react;
   };
 }
