efb6e83470f393455af9072d44333cca44db3c52
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-12-06 11:47:19 +0200

    Simplify Thread FTD

--- experiments/thread_ftd/src/main.c
+++ experiments/thread_ftd/src/main.c
@@ -1,130 +1,47 @@
-#include "app_scheduler.h"
-#include "app_timer.h"
-#include "bsp_thread.h"
+#include <openthread/thread.h>
 #include "nrf_log_ctrl.h"
 #include "nrf_log.h"
 #include "nrf_log_default_backends.h"
 
 #include "thread_utils.h"
 
-#include <openthread/thread.h>
-
-#define SCHED_QUEUE_SIZE      32                              /**< Maximum number of events in the scheduler queue. */
-#define SCHED_EVENT_DATA_SIZE APP_TIMER_SCHED_EVENT_DATA_SIZE /**< Maximum app_scheduler event size. */
-
-/***************************************************************************************************
- * @section State
- **************************************************************************************************/
-
-static void thread_state_changed_callback(uint32_t flags, void * p_context)
-{
-    NRF_LOG_INFO("State changed! Flags: 0x%08x Current role: %d\r\n",
-                 flags, otThreadGetDeviceRole(p_context));
+static void thread_state_changed_callback(uint32_t flags, void *p_context) {
+  NRF_LOG_INFO("State changed! Flags: 0x%08x Current role: %d",
+               flags, otThreadGetDeviceRole(p_context));
 }
 
-/***************************************************************************************************
- * @section Initialization
- **************************************************************************************************/
+static void thread_instance_init(void) {
+  thread_configuration_t thread_configuration =
+                             {
+                                 .role                  = RX_ON_WHEN_IDLE,
+                                 .autocommissioning     = true,
+                                 .autostart_disable     = false,
+                             };
 
-/**@brief Function for initializing the Application Timer Module.
- */
-static void timer_init(void)
-{
-    uint32_t err_code = app_timer_init();
-    APP_ERROR_CHECK(err_code);
+  thread_init(&thread_configuration);
+  thread_cli_init();
+  thread_state_changed_callback_set(thread_state_changed_callback);
 }
 
 
-/**@brief Function for initializing the LEDs.
- */
-static void leds_init(void)
-{
-    LEDS_CONFIGURE(LEDS_MASK);
-    LEDS_OFF(LEDS_MASK);
-}
 
+int main(int argc, char *argv[]) {
+  APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+  NRF_LOG_DEFAULT_BACKENDS_INIT();
 
-/**@brief Function for initializing the nrf log module.
- */
-static void log_init(void)
-{
-    ret_code_t err_code = NRF_LOG_INIT(NULL);
-    APP_ERROR_CHECK(err_code);
+  NRF_LOG_INFO("Starting Thread FTD..")
 
-    NRF_LOG_DEFAULT_BACKENDS_INIT();
-}
+  while (true) {
+    thread_instance_init();
 
+    while (!thread_soft_reset_was_requested()) {
+      thread_process();
 
-/**@brief Function for initializing the Thread Stack.
- */
-static void thread_instance_init(void)
-{
-    thread_configuration_t  thread_configuration =
-    {
-        .role                  = RX_ON_WHEN_IDLE,
-        .autocommissioning     = true,
-        .autostart_disable     = false,
-    };
-
-    thread_init(&thread_configuration);
-    thread_cli_init();
-    thread_state_changed_callback_set(thread_state_changed_callback);
-
-    uint32_t err_code = bsp_thread_init(thread_ot_instance_get());
-    APP_ERROR_CHECK(err_code);
-}
-
-
-/**@brief Function for deinitializing the Thread Stack.
- *
- */
-static void thread_instance_finalize(void)
-{
-    bsp_thread_deinit(thread_ot_instance_get());
-    thread_soft_deinit();
-}
-
-
-/**@brief Function for initializing scheduler module.
- */
-static void scheduler_init(void)
-{
-    APP_SCHED_INIT(SCHED_EVENT_DATA_SIZE, SCHED_QUEUE_SIZE);
-}
-
-/***************************************************************************************************
- * @section Main
- **************************************************************************************************/
-
-int main(int argc, char *argv[])
-{
-    log_init();
-    scheduler_init();
-    timer_init();
-    leds_init();
-
-    uint32_t err_code = bsp_init(BSP_INIT_LEDS, NULL);
-    APP_ERROR_CHECK(err_code);
-
-    while (true)
-    {
-        thread_instance_init();
-
-        while (!thread_soft_reset_was_requested())
-        {
-            thread_process();
-            app_sched_execute();
-
-            if (NRF_LOG_PROCESS() == false)
-            {
-                thread_sleep();
-            }
-        }
-
-        thread_instance_finalize();
+      if (NRF_LOG_PROCESS() == false) {
+        thread_sleep();
+      }
     }
-}
 
-/**
- *@}
- **/
+    thread_soft_deinit();
+  }
+}
