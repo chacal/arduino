1caccb62a3ec255595bf80dc0ff4fe46bd6ea42e
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-12-09 12:26:39 +0200

    Increase logger buffer size

--- thread/router/src/sdk_config.h
+++ thread/router/src/sdk_config.h
@@ -12213,7 +12213,7 @@
 // <i> or this value is actually used. It depends on which one is bigger.
 
 #ifndef SEGGER_RTT_CONFIG_BUFFER_SIZE_UP
-#define SEGGER_RTT_CONFIG_BUFFER_SIZE_UP 2048
+#define SEGGER_RTT_CONFIG_BUFFER_SIZE_UP 4096
 #endif
 
 // <o> SEGGER_RTT_CONFIG_MAX_NUM_UP_BUFFERS - Size of upstream buffer. 
