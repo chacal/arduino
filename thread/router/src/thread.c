8a9db99ccd50eb169479351f6fbbe42231286e22
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-12-09 12:27:00 +0200

    Pretty print IPv6 addresses

--- thread/router/src/thread.c
+++ thread/router/src/thread.c
@@ -21,11 +21,20 @@ static otSemanticallyOpaqueIidGeneratorData data = {
     .mSecretKeyLength = sizeof(ip6_identity)
 };
 
+static void print_ipv6_address(const otNetifAddress *address) {
+  char          buf[40];
+  const uint8_t *m8 = address->mAddress.mFields.m8;
+  sprintf(buf, "%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x",
+          m8[0], m8[1], m8[2], m8[3], m8[4], m8[5], m8[6], m8[7], m8[8], m8[9], m8[10], m8[11], m8[12], m8[13], m8[14], m8[15]
+  );
+  NRF_LOG_INFO("%s", buf)
+}
+
 static void print_addresses() {
   if (LOG_IP6_ADDRESSES) {
     const otNetifAddress *addr = (otIp6GetUnicastAddresses(thread_ot_instance_get()));
     for (; addr; addr = addr->mNext) {
-      NRF_LOG_HEXDUMP_INFO(addr->mAddress.mFields.m32, sizeof(addr->mAddress.mFields.m32))
+      print_ipv6_address(addr);
     }
   }
 }
