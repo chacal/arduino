e582bb45403f21d031e61a434bffe34b228f9324
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-02-13 23:50:15 +0200

    Don’t show VCC anymore
    VCC can be rendered remotely from the server

--- thread/network-display/src/states/connected.hpp
+++ thread/network-display/src/states/connected.hpp
@@ -8,7 +8,6 @@
 #include "display_command_handler.hpp"
 
 #define COAP_TICK_PERIOD      std::chrono::seconds(1)
-#define VCC_DISPLAY_PERIOD    std::chrono::seconds(10)
 
 using namespace fsm;
 
@@ -16,12 +15,10 @@ namespace states {
 
   struct connected : Base {
     struct coap_timer_ticked {};
-    struct vcc_display_timer_ticked {};
 
     virtual void enter(Context &context) {
       NRF_LOG_INFO("Connected");
       coap_tick_timer.start(&context);
-      vcc_display_timer.start(&context);
       context.disp.on();
 
       auto on_coap_post = [&](const coap_service::post_data &coap_data) { context.react(coap_data); };
@@ -33,13 +30,6 @@ namespace states {
 
     virtual void react(const coap_timer_ticked &event, Control &control, Context &context) {
       coap_time_tick();
-    }
-
-    virtual void react(const vcc_display_timer_ticked &event, Control &control, Context &context) {
-      auto vcc = vcc::measure();
-      auto idx = static_cast<uint8_t >(context.display_list.max_length - 1);
-      context.display_list.add(commands::str_cmd{idx, point(0, 64), font_size(8), std::string{std::to_string(vcc) + "mV"}});
-      context.display_list.render(context.disp);
     }
 
     virtual void react(const coap_service::post_data &data, Control &control, Context &context) {
@@ -63,7 +53,6 @@ namespace states {
 
   private:
     periodic_timer coap_tick_timer{COAP_TICK_PERIOD, [](void *ctx) { static_cast<Context *>(ctx)->react(coap_timer_ticked{}); }};
-    periodic_timer vcc_display_timer{VCC_DISPLAY_PERIOD, [](void *ctx) { static_cast<Context *>(ctx)->react(vcc_display_timer_ticked{}); }};
   };
 }
 
