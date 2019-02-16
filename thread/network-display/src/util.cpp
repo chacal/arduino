99fc500d295caad8298ef96511259ae254a19369
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-02-16 20:02:18 +0200

    Add instance information to status response

--- thread/network-display/src/util.cpp
+++ thread/network-display/src/util.cpp
@@ -2,6 +2,8 @@
 #include "ArduinoJson-v5.13.4.hpp"
 #include "vcc.hpp"
 
+#define DEVICE_NAME         "D100"
+
 using namespace ArduinoJson;
 
 namespace util {
@@ -9,7 +11,8 @@ namespace util {
   std::string get_status_json() {
     StaticJsonBuffer<100> jsonBuffer;
     JsonObject            &root = jsonBuffer.createObject();
-    root["vcc"] = vcc::measure();
+    root["vcc"]      = vcc::measure();
+    root["instance"] = DEVICE_NAME;
     std::string output;
     root.printTo(output);
     return output;
