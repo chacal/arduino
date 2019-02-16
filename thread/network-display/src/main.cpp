ae917e5442f6c98c1273b09d61f78bb21a6c712d
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-02-16 19:53:18 +0200

    Move power supply initialisation to power_manager

--- thread/network-display/src/main.cpp
+++ thread/network-display/src/main.cpp
@@ -3,30 +3,12 @@
 #include <nrf_log.h>
 #include <nrf_log_default_backends.h>
 #include <app_scheduler.h>
-#include <nrf_power.h>
 #include "thread.hpp"
 #include "state_machine.hpp"
-
-// Set REG0 (high voltage DC/DC regulator) to output 3V. This becomes board's VDD and is supplied to external peripherals & GPIO.
-volatile uint32_t m_uicr_regout0 __attribute__ ((section(".uicr_regout0"))) = UICR_REGOUT0_VOUT_3V0;
-
-uint32_t get_regout0() {
-  return NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk;
-}
-
-void initialize_power_supply() {
-  nrf_power_dcdcen_set(true);
-  nrf_power_dcdcen_vddh_set(true);
-  NRF_LOG_INFO("REGOUT0: %d", get_regout0())
-  NRF_LOG_INFO("Main reg status: %d", nrf_power_mainregstatus_get())
-  NRF_LOG_INFO("REG0 DCDC enabled: %d", nrf_power_dcdcen_vddh_get())
-  NRF_LOG_INFO("REG1 DCDC enabled: %d", nrf_power_dcdcen_get())
-}
 
 int main(int argc, char *argv[]) {
   APP_ERROR_CHECK(NRF_LOG_INIT(nullptr));
   NRF_LOG_DEFAULT_BACKENDS_INIT();
-  initialize_power_supply();
   APP_ERROR_CHECK(app_timer_init());  // Needs to be initialized before FSM as some states use app_timer in their constructors
 
   NRF_LOG_INFO("Starting Network Display "
--- thread/network-display/src/power_manager.cpp
+++ thread/network-display/src/power_manager.cpp
@@ -2,13 +2,32 @@
 
 #include <app_error.h>
 #include <nrf_log.h>
+#include <nrf_power.h>
 
 extern "C" {
 #include <nrf_pwr_mgmt.h>
 }
 
+// Set REG0 (high voltage DC/DC regulator) to output 3V. This becomes board's VDD and is supplied to external peripherals & GPIO.
+volatile uint32_t m_uicr_regout0 __attribute__ ((section(".uicr_regout0"))) = UICR_REGOUT0_VOUT_3V0;
+
 namespace power_manager {
+
+  uint32_t get_regout0() {
+    return NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk;
+  }
+
+  void initialize_power_supply() {
+    nrf_power_dcdcen_set(true);
+    nrf_power_dcdcen_vddh_set(true);
+    NRF_LOG_INFO("REGOUT0: %d", get_regout0())
+    NRF_LOG_INFO("Main reg status: %d", nrf_power_mainregstatus_get())
+    NRF_LOG_INFO("REG0 DCDC enabled: %d", nrf_power_dcdcen_vddh_get())
+    NRF_LOG_INFO("REG1 DCDC enabled: %d", nrf_power_dcdcen_get())
+  }
+
   void init() {
+    initialize_power_supply();
     uint32_t error = nrf_pwr_mgmt_init();
     APP_ERROR_CHECK(error);
   }
