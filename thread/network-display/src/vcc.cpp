b34262bb1f8c28a727ac2ac34a68cc5ff3652257
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-06-04 20:19:53 +0300

    Measure VDD (or VDDH) using the internal reference

--- thread/network-display/src/vcc.cpp
+++ thread/network-display/src/vcc.cpp
@@ -1,8 +1,6 @@
 #include <nrfx_saadc.h>
+#include <nrf_power.h>
 #include "vcc.hpp"
-
-#define R1   3300     // kΩ
-#define R2    910     // kΩ
 
 static void on_adc_event(nrfx_saadc_evt_t const *event) {}
 
@@ -13,8 +11,16 @@ namespace vcc {
     nrfx_saadc_config_t config = NRFX_SAADC_DEFAULT_CONFIG;
     APP_ERROR_CHECK(nrfx_saadc_init(&config, on_adc_event));
 
-    nrf_saadc_channel_config_t channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN5);
-    channel_config.gain  = NRF_SAADC_GAIN1_2;
+    nrf_saadc_channel_config_t channel_config;
+
+    if (nrf_power_mainregstatus_get() == NRF_POWER_MAINREGSTATUS_NORMAL) {
+      channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_VDD);
+      channel_config.gain = NRF_SAADC_GAIN1_6;
+    } else {
+      channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(SAADC_CH_PSELP_PSELP_VDDHDIV5);
+      channel_config.gain = NRF_SAADC_GAIN1_2;
+    }
+
     channel_config.burst = NRF_SAADC_BURST_ENABLED;
     APP_ERROR_CHECK(nrfx_saadc_channel_init(m_adc_channel, &channel_config));
   }
@@ -23,10 +29,14 @@ namespace vcc {
     nrf_saadc_value_t value;
     nrfx_saadc_sample_convert(m_adc_channel, &value);
 
-    float resistor_divider_gain = (R1 + R2) / (float) R2;
-
-    // ADC value (12-bit) * prescale (1/2) * 0.6V reference * mV * PCB resistor divider
-    auto vcc = (uint16_t) (value / (float) 4095 * 2 * 0.6 * 1000 * resistor_divider_gain);
-    return vcc;
+    if (nrf_power_mainregstatus_get() == NRF_POWER_MAINREGSTATUS_NORMAL) {
+      // ADC value (12-bit) * gain (1/6) * 0.6V reference * mV
+      auto vcc = (uint16_t) (value / (float) 4095 * 6 * 0.6 * 1000);
+      return vcc;
+    } else {
+      // ADC value (12-bit) * gain (1/2) * vddh/5 * 0.6V reference * mV
+      auto vcc = (uint16_t) (value / (float) 4095 * 2 * 5 * 0.6 * 1000);
+      return vcc;
+    }
   }
 }
