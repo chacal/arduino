fb36eb219d7dcf75613d4df1336ead7720544c62
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-07-20 12:39:22 +0200

    Add support for /api/image CoAP endpoint that accepts gzipped binary images and renders them

--- thread/network-display/src/coap_service.cpp
+++ thread/network-display/src/coap_service.cpp
@@ -8,14 +8,19 @@
 namespace coap_service {
 
   static request_handler m_request_handler;
-  static coap_resource_t m_api_resource      = {};
-  static coap_resource_t m_display_resource  = {};
-  static coap_resource_t m_status_resource   = {};
-  static coap_resource_t m_settings_resource = {};
+  static coap_resource_t m_api_resource           = {};
+  static coap_resource_t m_display_resource       = {};
+  static coap_resource_t m_display_image_resource = {};
+  static coap_resource_t m_status_resource        = {};
+  static coap_resource_t m_settings_resource      = {};
 
 
   static void on_display_post(coap_resource_t *p_resource, coap_message_t *p_request) {
     coap_helpers::handle_json_post(p_resource, p_request, m_request_handler.on_display_post);
+  }
+
+  static void on_display_image_post(coap_resource_t *p_resource, coap_message_t *p_request) {
+    coap_helpers::handle_binary_post(p_resource, p_request, m_request_handler.on_display_image_post);
   }
 
   static void on_status_get(coap_resource_t *p_resource, coap_message_t *p_request) {
@@ -42,6 +47,14 @@ namespace coap_service {
     m_display_resource.callback        = on_display_post;
   }
 
+  void initialize_display_image_resource() {
+    coap_helpers::create_resource(m_display_image_resource, "image");
+
+    m_display_image_resource.permission      = COAP_PERM_POST;
+    m_display_image_resource.ct_support_mask = COAP_CT_MASK_APP_OCTET_STREAM;
+    m_display_image_resource.callback        = on_display_image_post;
+  }
+
   void initialize_status_resource() {
     coap_helpers::create_resource(m_status_resource, "status");
 
@@ -62,10 +75,12 @@ namespace coap_service {
     m_request_handler = handler;
     initialize_api_resource();
     initialize_display_resource();
+    initialize_display_image_resource();
     initialize_status_resource();
     initialize_settings_resource();
 
     APP_ERROR_CHECK(coap_resource_child_add(&m_api_resource, &m_display_resource));
+    APP_ERROR_CHECK(coap_resource_child_add(&m_api_resource, &m_display_image_resource));
     APP_ERROR_CHECK(coap_resource_child_add(&m_api_resource, &m_status_resource));
     APP_ERROR_CHECK(coap_resource_child_add(&m_api_resource, &m_settings_resource));
     coap_helpers::add_resource_to_root(m_api_resource);
--- thread/network-display/src/states/connected.hpp
+++ thread/network-display/src/states/connected.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <variant>
 #include <nrf_log.h>
 #include <settings.hpp>
 #include "periodic_timer.hpp"
@@ -7,6 +8,7 @@
 #include "coap_service.hpp"
 #include "cmd_protocol.hpp"
 #include "display_command_handler.hpp"
+#include "gzip.hpp"
 
 #define COAP_TICK_PERIOD      std::chrono::seconds(1)
 
@@ -22,9 +24,11 @@ namespace states {
       coap_tick_timer.start(&context);
       context.disp->on();
 
-      auto on_display_post = [&](auto coap_data) { handle_display_post(coap_data, context); };
+      auto on_display_post       = [&](auto coap_data) { handle_display_post(coap_data, context); };
+      auto on_display_image_post = [&](auto coap_data) { handle_display_image_post(coap_data, context); };
       coap_service::initialize({
                                    on_display_post,
+                                   on_display_image_post,
                                    &settings::get_as_json,
                                    &settings::update,
                                    &util::get_status_json,
@@ -50,6 +54,20 @@ namespace states {
           std::visit(display_command_handler{context.display_list}, cmd);
         }
         context.display_list.render(*context.disp);
+      }
+    }
+
+    void handle_display_image_post(const coap_service::post_data &coap_data, Context &context) {
+      NRF_LOG_DEBUG("Got %d bytes of compressed image data", coap_data.len)
+      auto result = gzip::uncompress(coap_data.data, coap_data.len);
+      if (std::holds_alternative<std::vector<uint8_t>>(result)) {
+        auto uncompressed = std::get<std::vector<uint8_t>>(result);
+        NRF_LOG_DEBUG("Uncompressed image size: %d bytes", uncompressed.size())
+        context.disp->draw_fullscreen_bitmap(uncompressed);
+        context.disp->render();
+      } else {
+        auto error_code = std::get<int>(result);
+        NRF_LOG_ERROR("Error uncompressing image! Error code: %d", error_code)
       }
     }
   };
