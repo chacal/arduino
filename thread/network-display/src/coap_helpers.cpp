eb00a6fb262b38240f7b62357a13c4e74a2863b2
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-06-30 19:28:04 +0300

    Add support for receiving CoAP Block1 requests

--- thread/network-display/src/coap_helpers.cpp
+++ thread/network-display/src/coap_helpers.cpp
@@ -2,6 +2,8 @@
 #include <app_error.h>
 #include <coap_api.h>
 #include <coap_message.h>
+#include <coap_block.h>
+#include <nrf_log.h>
 
 #include "coap_helpers.hpp"
 
@@ -63,20 +65,76 @@ namespace coap_helpers {
     APP_ERROR_CHECK(err_code);
   }
 
+  coap_block_opt_block1_t get_opt_block1(coap_message_t *p_request) {
+    uint8_t option_index;
+    coap_message_opt_index_get(&option_index, p_request, COAP_OPT_BLOCK1);
+    coap_option_t *p_block1_opt = &p_request->options[option_index];
+
+    // Decode option value into a uint32_t.
+    uint32_t block1_opt_value_raw;
+    APP_ERROR_CHECK(coap_opt_uint_decode(&block1_opt_value_raw, p_block1_opt->length, p_block1_opt->p_data));
+
+    // Decode uint32_t value into block1 option structure.
+    coap_block_opt_block1_t block1_request;
+    APP_ERROR_CHECK(coap_block_opt_block1_decode(&block1_request, block1_opt_value_raw));
+
+    return block1_request;
+  }
+
+  uint32_t get_encoded_block1_response_opt(const coap_block_opt_block1_t &block1_request_opt) {
+    coap_block_opt_block1_t block1_response = {
+      .more   = block1_request_opt.more,
+      .size   = block1_request_opt.size,
+      .number = block1_request_opt.number
+    };
+
+    uint32_t block1_response_encoded;
+    APP_ERROR_CHECK(coap_block_opt_block1_encode(&block1_response_encoded, &block1_response));
+    return block1_response_encoded;
+  }
+
+  std::vector<uint8_t> msg_buf;
+
+  void handle_block1_post(coap_message_t *p_request, const coap_service::post_handler &handler) {
+    auto block1_request_opt = get_opt_block1(p_request);
+    NRF_LOG_DEBUG("Block1: payload: %dB, num: %d, more: %d", p_request->payload_len, block1_request_opt.number, block1_request_opt.more)
+
+    if (block1_request_opt.number == 0) {
+      msg_buf.clear();
+    }
+    msg_buf.insert(msg_buf.end(), &p_request->p_payload[0], &p_request->p_payload[p_request->payload_len]);
+
+    auto     response_status             = block1_request_opt.more == 1 ? COAP_CODE_231_CONTINUE : COAP_CODE_204_CHANGED;
+    auto     p_response                  = create_response_for(p_request, response_status);
+    uint32_t block1_response_opt_encoded = get_encoded_block1_response_opt(block1_request_opt);
+    APP_ERROR_CHECK(coap_message_opt_uint_add(p_response, COAP_OPT_BLOCK1, block1_response_opt_encoded));
+
+    send_and_delete(p_response);
+
+    if (block1_request_opt.more == COAP_BLOCK_OPT_BLOCK_MORE_BIT_UNSET) {
+      NRF_LOG_DEBUG("Block1 complete: total payload: %dB", msg_buf.size())
+      handler({msg_buf.data(), (uint16_t) msg_buf.size()});
+      msg_buf.clear();
+    }
+  }
+
   void handle_post(coap_resource_t *p_resource, coap_message_t *p_request, const coap_service::post_handler &handler,
                    uint32_t content_type_mask) {
     if (has_content_type(p_request, content_type_mask)) {
-      // Create a temporary copy of the payload in order to respond and release the request before actually processing it
-      uint8_t temp[p_request->payload_len];
-      memcpy(temp, p_request->p_payload, p_request->payload_len);
-      auto len = p_request->payload_len;
+      if (coap_message_opt_present(p_request, COAP_OPT_BLOCK1) == NRF_SUCCESS) {
+        handle_block1_post(p_request, handler);
+      } else {
+        // Create a temporary copy of the payload in order to respond and release the request before actually processing it
+        msg_buf.clear();
+        msg_buf.insert(msg_buf.end(), &p_request->p_payload[0], &p_request->p_payload[p_request->payload_len]);
 
-      // Respond
-      auto p_response = create_response_for(p_request, COAP_CODE_204_CHANGED);
-      send_and_delete(p_response);
+        // Respond
+        auto p_response = create_response_for(p_request, COAP_CODE_204_CHANGED);
+        send_and_delete(p_response);
 
-      // Run handler
-      handler({temp, len});
+        // Run handler
+        handler({msg_buf.data(), (uint16_t) msg_buf.size()});
+      }
     } else {
       auto p_response = create_response_for(p_request, COAP_CODE_415_UNSUPPORTED_CONTENT_FORMAT);
       send_and_delete(p_response);
