0b95319ca065c72719b28c1eebbf0f78e00de515
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-09-12 20:10:10 +0300

    Configure buttons according to real hardware

--- nrf5-autopilot-remote/src/buttons.cpp
+++ nrf5-autopilot-remote/src/buttons.cpp
@@ -4,9 +4,12 @@
 #include <unordered_map>
 #include "Button.hpp"
 
-#define BUTTON_PIN1          13
-#define BUTTON_PIN2          14
-#define BUTTON_PIN3          15
+#define BUTTON_ENABLE_PIN       2
+#define BUTTON_DISABLE_PIN      3
+#define BUTTON_MINUS10_PIN     11
+#define BUTTON_MINUS1_PIN      12
+#define BUTTON_PLUS1_PIN       13
+#define BUTTON_PLUS10_PIN      14
 #define BUTTON_DEBOUNCE_MS   APP_TIMER_TICKS(10)
 
 std::unordered_map<uint8_t, Button> buttons;
@@ -18,17 +21,23 @@ void on_button(uint8_t pin_no, uint8_t button_action) {
 
 void buttons_init(button_cb_t push_cb, button_cb_t release_cb) {
   buttons                           = {
-    {BUTTON_PIN1, Button(BTN_ENABLE, push_cb, release_cb)},
-    {BUTTON_PIN2, Button(BTN_DISABLE, push_cb, release_cb)},
-    {BUTTON_PIN3, Button(BTN_MINUS10, push_cb, release_cb)},
+    {BUTTON_ENABLE_PIN,  Button(BTN_ENABLE, push_cb, release_cb)},
+    {BUTTON_DISABLE_PIN, Button(BTN_DISABLE, push_cb, release_cb)},
+    {BUTTON_MINUS10_PIN, Button(BTN_MINUS10, push_cb, release_cb)},
+    {BUTTON_MINUS1_PIN, Button(BTN_MINUS1, push_cb, release_cb)},
+    {BUTTON_PLUS1_PIN, Button(BTN_PLUS1, push_cb, release_cb)},
+    {BUTTON_PLUS10_PIN, Button(BTN_PLUS10, push_cb, release_cb)},
   };
 
   APP_ERROR_CHECK(app_timer_init());
   static app_button_cfg_t buttons[] =
                             {
-                              {BUTTON_PIN1, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
-                              {BUTTON_PIN2, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
-                              {BUTTON_PIN3, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
+                              {BUTTON_ENABLE_PIN,  APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
+                              {BUTTON_DISABLE_PIN, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
+                              {BUTTON_MINUS10_PIN, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
+                              {BUTTON_MINUS1_PIN, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
+                              {BUTTON_PLUS1_PIN, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
+                              {BUTTON_PLUS10_PIN, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, on_button},
                             };
 
   APP_ERROR_CHECK(app_button_init(buttons, ARRAY_SIZE(buttons), BUTTON_DEBOUNCE_MS));
