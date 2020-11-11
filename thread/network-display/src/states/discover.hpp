8f2bcd53127f5ba4fa2f20a131e5f48389b1e5d4
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-11-11 18:49:45 +0200

    Delay configuring by 3 seconds to let Thread network setup routing properly
    Without the delay response packages from mgmt server won't arrive at display

--- thread/network-display/src/states/discover.hpp
+++ thread/network-display/src/states/discover.hpp
@@ -14,6 +14,7 @@
 #define DISCOVERY_SERVER_ADDR  "ff03::1"
 #define DISCOVERY_SERVER_PORT  8000
 #define DISCOVERY_SERVER_PATH  "discover"
+#define CONFIGURE_DELAY        std::chrono::seconds(3)
 
 using namespace fsm;
 
@@ -23,6 +24,8 @@ namespace states {
     struct mgmt_server_discovery_timer_ticked {
     };
     struct mgmt_server_set {
+    };
+    struct configure_delay_elapsed {
     };
 
     virtual void enter(Context &context) {
@@ -35,8 +38,12 @@ namespace states {
     }
 
     virtual void react(const mgmt_server_set &event, Control &control, Context &context) {
-      NRF_LOG_INFO("Using mgmt server: %s", context.mgmt_server_address.c_str())
+      NRF_LOG_INFO("Discovered mgmt server! %s", context.mgmt_server_address.c_str())
       mgmt_server_discovery_timer.stop();
+      configure_delay_timer.start(&context);
+    }
+
+    virtual void react(const configure_delay_elapsed &event, Control &control, Context &context) {
       control.changeTo<configure>();
     }
 
@@ -46,6 +53,9 @@ namespace states {
   private:
     periodic_timer mgmt_server_discovery_timer{MGMT_DISCOVERY_PERIOD, [](void *ctx) {
       static_cast<Context *>(ctx)->react(mgmt_server_discovery_timer_ticked{});
+    }};
+    oneshot_timer  configure_delay_timer{CONFIGURE_DELAY, [](void *ctx) {
+      static_cast<Context *>(ctx)->react(configure_delay_elapsed{});
     }};
 
     void discover_mgmt_server(Context &context) {
