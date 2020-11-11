3b6acfbc7ce4ceabfc3d9b500f2ca106c95002b0
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-11-11 18:38:56 +0200

    Move coap tick timer from start state to connected state

--- thread/network-display/src/states/connected.hpp
+++ thread/network-display/src/states/connected.hpp
@@ -7,11 +7,16 @@
 #include "common.hpp"
 #include "gzip.hpp"
 
+#define COAP_TICK_PERIOD       std::chrono::seconds(1)
+
 using namespace fsm;
 
 namespace states {
 
   struct connected : Base {
+    struct coap_timer_ticked {
+    };
+
     virtual void enter(Context &context) {
       NRF_LOG_INFO("Connected");
       context.disp->on();
@@ -23,12 +28,19 @@ namespace states {
                                  &settings::update,
                                  &util::get_status_json,
                                });
+      coap_tick_timer.start(&context);
+    }
+
+    virtual void react(const coap_timer_ticked &event, Control &control, Context &context) {
+      coap_time_tick();
     }
 
     using Base::react;
 
 
   private:
+    periodic_timer coap_tick_timer{COAP_TICK_PERIOD, [](void *ctx) { static_cast<Context *>(ctx)->react(coap_timer_ticked{}); }};
+
     void handle_display_image_post(const coap_service::post_data &coap_data, Context &context) {
       NRF_LOG_DEBUG("Got %d bytes of compressed image data", coap_data.len)
       auto result = gzip::uncompress(coap_data.data, coap_data.len);
--- thread/network-display/src/states/start.hpp
+++ thread/network-display/src/states/start.hpp
@@ -14,8 +14,6 @@ extern "C" {
 #include "power_manager.hpp"
 #include "vcc.hpp"
 
-#define COAP_TICK_PERIOD       std::chrono::seconds(1)
-
 using namespace fsm;
 
 namespace states {
@@ -23,9 +21,6 @@ namespace states {
   using thread_role = named_type<otDeviceRole, struct thread_role_param>;
 
   struct start : Base {
-    struct coap_timer_ticked {
-    };
-
     virtual void enter(Context &context) {
       APP_ERROR_CHECK(nrf_mem_init());
       power_manager::init();
@@ -37,7 +32,6 @@ namespace states {
 
       NRF_LOG_INFO("Start: Initializing DFU");
       APP_ERROR_CHECK(coap_dfu_init(ot));
-      coap_tick_timer.start(&context);
     }
 
     virtual void react(const thread_role &role, Control &control, Context &context) {
@@ -47,13 +41,6 @@ namespace states {
       }
     }
 
-    virtual void react(const coap_timer_ticked &event, Control &control, Context &context) {
-      coap_time_tick();
-    }
-
     using Base::react;
-
-  private:
-    periodic_timer coap_tick_timer{COAP_TICK_PERIOD, [](void *ctx) { static_cast<Context *>(ctx)->react(coap_timer_ticked{}); }};
   };
 }
