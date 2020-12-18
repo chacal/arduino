7965b2450538157c2f19ec22f0933d46413299b7
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-12-18 19:34:03 +0200

    Retry getting configuration on failures

--- thread/network-display/src/states/configure.hpp
+++ thread/network-display/src/states/configure.hpp
@@ -16,17 +16,29 @@ namespace states {
     };
     struct config_set {
     };
+    struct coap_timer_ticked {
+    };
 
     virtual void enter(Context &context) {
       NRF_LOG_INFO("Configuring..");
       config_timer.start(&context);
+      coap_tick_timer.start(&context);
       get_configuration(context);
+    }
+
+    virtual void leave(Context &context)  {
+      config_timer.stop();
+      coap_tick_timer.stop();
     }
 
     virtual void react(const config_set &event, Control &control, Context &context) {
       NRF_LOG_INFO("Configured:")
       NRF_LOG_INFO("%s", settings::get_as_json().c_str())
       control.changeTo<connected>();
+    }
+
+    virtual void react(const coap_timer_ticked &event, Control &control, Context &context) {
+      coap_time_tick();
     }
 
     virtual void react(const config_timer_elapsed &event, Control &control, Context &context) {
@@ -36,6 +48,7 @@ namespace states {
     using Base::react;
 
   private:
+    periodic_timer coap_tick_timer{COAP_TICK_PERIOD, [](void *ctx) { static_cast<Context *>(ctx)->react(coap_timer_ticked{}); }};
     periodic_timer config_timer{CONFIG_TIMER_PERIOD, [](void *ctx) { static_cast<Context *>(ctx)->react(config_timer_elapsed{}); }};
 
     void get_configuration(Context &context) {
@@ -45,10 +58,12 @@ namespace states {
       auto responseHandler = [](uint32_t status, void *p_arg, coap_message_t *p_message) {
         if (status != NRF_SUCCESS) {
           NRF_LOG_ERROR("Error status from config response. Status: %d", status)
-          return;
+        } else if(p_message->header.code != COAP_CODE_205_CONTENT) {
+          NRF_LOG_ERROR("Unexpected management server response code: %d", p_message->header.code)
+        } else {
+          settings::update(p_message->p_payload, p_message->payload_len);
+          static_cast<Context *>(p_arg)->react(config_set{});
         }
-        settings::update(p_message->p_payload, p_message->payload_len);
-        static_cast<Context *>(p_arg)->react(config_set{});
       };
 
       coap_helpers::get(settings::m_mgmt_server, MGMT_SERVER_PORT, "v1/devices/" + id, responseHandler, &context);
